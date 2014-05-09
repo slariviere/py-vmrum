b8bf868d6ae7dbeb695dac36d5f72231d429d180
Sébastien Larivière <sebastien@lariviere.me>
2014-05-08 21:55:09 -0400

    Delete escped variable, replaced with single quotes

--- clone-vm.py
+++ clone-vm.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-import os
-import sys
+import os, sys
 
 # Set arguments values
 if len(sys.argv) == 1:
@@ -10,19 +9,17 @@ if len(sys.argv) == 1:
 else:
    new_vm_name = sys.argv[1]
 
-vms_home_dir = os.getenv('HOME') + '/Documents/Virtual\ Machines.localized/'
-vms_home_dir_not_escaped = os.getenv('HOME') + '/Documents/Virtual Machines.localized/'
+vms_path_dir = os.getenv('HOME') + '/Documents/Virtual Machines.localized/'
 
 vm_source_vmx = vms_home_dir + 'base-centos-64.vmwarevm/base-centos-64.vmx'
 
-vm_dest_dir_not_escaped = vms_home_dir_not_escaped + new_vm_name + ".vmwarevm"
-vm_dest_dir = vms_home_dir + new_vm_name
+vm_dest_dir = vms_home_dir + new_vm_name + ".vmwarevm"
 vm_dest_vmx = vm_dest_dir + '/' + new_vm_name + '.vmx'
 
-if not os.path.exists(vm_dest_dir_not_escaped):
-  os.makedirs(vm_dest_dir_not_escaped)
+if not os.path.exists(vm_dest_dir):
+  os.makedirs(vm_dest_dir)
 
-cmd = 'vmrun clone ' + vm_source_vmx + ' ' + vm_dest_vmx + ' linked -cloneName=' + new_vm_name  
+cmd = 'vmrun clone \'' + vm_source_vmx + '\' \'' + vm_dest_vmx + '\' linked -cloneName=' + new_vm_name  
 
-print "[+] Creating new linked vm"
+print "[+] Creating new linked vm: " + new_vm_name
 os.system(cmd)
