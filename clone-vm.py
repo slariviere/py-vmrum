27ff64a929b867d9735e55c83a16494e9111b7ae
Sébastien Larivière <sebastien@lariviere.me>
2014-05-08 21:44:08 -0400

    Basic argument validation

--- clone-vm.py
+++ clone-vm.py
@@ -3,19 +3,26 @@
 import os
 import sys
 
+# Set arguments values
+if len(sys.argv) == 1:
+   print "Usage: clone-vm.py [new-vm-name]"
+   exit(1)
+else:
+   new_vm_name = sys.argv[1]
+
 vms_home_dir = os.getenv('HOME') + '/Documents/Virtual\ Machines.localized/'
 vms_home_dir_not_escaped = os.getenv('HOME') + '/Documents/Virtual Machines.localized/'
 
 vm_source_vmx = vms_home_dir + 'base-centos-64.vmwarevm/base-centos-64.vmx'
 
-vm_dest_dir_not_escaped = vms_home_dir_not_escaped + sys.argv[1]
-vm_dest_dir = vms_home_dir + sys.argv[1]
-vm_dest_vmx = vm_dest_dir + '/' + sys.argv[1] + '.vmx'
+vm_dest_dir_not_escaped = vms_home_dir_not_escaped + new_vm_name + ".vmwarevm"
+vm_dest_dir = vms_home_dir + new_vm_name
+vm_dest_vmx = vm_dest_dir + '/' + new_vm_name + '.vmx'
 
 if not os.path.exists(vm_dest_dir_not_escaped):
   os.makedirs(vm_dest_dir_not_escaped)
 
-cmd = 'vmrun clone ' + vm_source_vmx + ' ' + vm_dest_vmx + ' linked -cloneName=' + sys.argv[1]  
+cmd = 'vmrun clone ' + vm_source_vmx + ' ' + vm_dest_vmx + ' linked -cloneName=' + new_vm_name  
 
-print cmd
+print "[+] Creating new linked vm"
 os.system(cmd)
