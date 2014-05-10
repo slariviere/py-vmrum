72fd23638f46bdf66577d39a007b802e0ab382e6
Sébastien Larivière <sebastien@lariviere.me>
2014-05-09 21:36:59 -0400

    Argument parsing

--- clone-vm.py
+++ clone-vm.py
@@ -1,29 +1,68 @@
 #!/usr/bin/env python
-
 import os, sys, yaml
 
+# Set the help menu
+def getSyntax():
+    return "Usage: clone-vm.py [options...] <vm_name>"
+
 # Set arguments values
+def setArguments(argv):
+    opts = {}
+    # Set available arguments
+    valued_args = { '--username': 'username', '-u': 'username', '--password': 'password', '-p': 'password' }
+    non_valued_args = { '--linked': 'linked', '-l': 'linked', '--full': 'full', '-f': 'full' }
+    while argv:
+        # If it's an option
+        if argv[0][0] == '-':
+            # if it's a valued argument we keep the value and cut 2 arguments form the list
+            if argv[0] in valued_args:
+                opts[valued_args[argv[0]]] = argv[1] 
+                argv = argv[2:]
+            # if it's not a valued argument we set it as true and cut 1 argument 
+            elif argv[0] in non_valued_args:
+                opts[non_valued_args[argv[0]]] = True 
+                argv = argv[1:]
+            # We don't have available definition for this argument, show the help
+            else:
+                print 'Invalid argument.'
+                print getSyntax()
+                exit(1)
+        # New vm name
+        elif len(argv) == 1:
+           opts['name'] = argv[0]
+           argv = argv[1:]
+        # There is a problem with the syntax
+        elif argv[0] != sys.argv[0]:
+            print 'Invalid argument.'
+            print getSyntax()
+            exit(1)
+        # This is the script name, we don't store it
+        elif argv[0] == sys.argv[0]:
+            argv = argv[1:]
+    return opts
+
 if len(sys.argv) == 1:
-   print "Usage: clone-vm.py [new-vm-name]"
+   print getSyntax() 
    exit(1)
 else:
-   new_vm_name = sys.argv[1]
+   args = setArguments(sys.argv)
 
+# Load yaml file configuration
 if os.path.isfile('config.yaml'):
     f = open('config.yaml')
     configMap = yaml.safe_load(f)
     f.close()
 else:
-    print "Config file 'config.yaml' not fourd"
+    print "Config file 'config.yaml' not found"
     exit(2)
 
-vm_dest_dir = configMap['vms']['path_dir'] + "/" + new_vm_name + ".vmwarevm"
-vm_dest_vmx = vm_dest_dir + '/' + new_vm_name + '.vmx'
+vm_dest_dir = configMap['vms']['path_dir'] + "/" + args['name'] + ".vmwarevm"
+vm_dest_vmx = vm_dest_dir + '/' + args['name'] + '.vmx'
 
 if not os.path.exists(vm_dest_dir):
   os.makedirs(vm_dest_dir)
 
-cmd = 'vmrun clone \'' + configMap['vm']['source']['vmx_path'] + '\' \'' + vm_dest_vmx + '\' linked -cloneName=' + new_vm_name  
+cmd = "vmrun clone '" + configMap['vm']['source']['vmx_path'] + "' '" + vm_dest_vmx + "' linked -cloneName=" + args['name']  
 
-print "[+] Creating new linked vm: " + new_vm_name
+print "[+] Creating new linked vm: " + args['name']
 os.system(cmd)
