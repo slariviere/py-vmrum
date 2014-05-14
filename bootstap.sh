5c1211b90094e00e72c670536c7fa152929abbe2
Sébastien Larivière <sebastien@lariviere.me>
2014-05-13 20:05:53 -0400

    Only link pyvmrun* files to the user bin directory

--- bootstap.sh
+++ bootstap.sh
@@ -3,7 +3,7 @@
 # Create symlinks in $HOME/bin to all python script in the current directory with the extention truncated
 
 echo "[+] Creating links :"
-for file in $(ls -1 *.py); do
+for file in $(ls -1 pyvmrun*.py); do
     dest_filename=$(echo $file | sed 's/.py$//')
     current_dir=$(pwd)
     # If the symlik exist, we rplace it
