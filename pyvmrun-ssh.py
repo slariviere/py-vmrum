7f88fa188dc8dc2b55db6d6e5bbcffc0682f4ff0
Sébastien Larivière <sebastien@lariviere.me>
2014-05-20 19:53:22 -0400

    Fast iteration of pyvmrun-ssh.py

---
+++ pyvmrun-ssh.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os, sys, config
+
+# Set the help menu
+def getSyntax():
+    return "Usage: pyvmrun-ssh <vm_name>"
+
+# Parsing arguments
+if len(sys.argv) == 1:
+    print getSyntax() 
+    exit(1)
+else:
+    conf = config.Config()
+    vm_start_dest = conf.getVmsPathDir() + "/" + sys.argv[1] +  ".vmwarevm/" +  sys.argv[1] + ".vmx"
+    if not os.path.exists(vm_start_dest):
+        print "Virtual machine not found."
+        exit(1)
+    cmd = "vmrun getGuestIPAddress '" + vm_start_dest + "'"
+    response = os.popen(cmd)
+    while 1:
+        line = response.readline()
+        if line != "":
+            ip=line
+        if not line: break
+    
+    #If the username is not as arguement, we use root
+    if len(sys.argv) >= 3:
+        username = sys.argv[2]
+    else:
+        username = "root"
+
+    cmd = "ssh " + username + "@" + ip
+    os.system(cmd)
