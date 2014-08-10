8880eb58efe4d93c794a8d5854502cae982a0e3b
Sébastien Larivière <sebastien@lariviere.me>
2014-08-10 14:20:35 -0400

    Only do the switch if the new mac is not aleready in the ifcfg file

--- scripts/fix-eth0-on-clone.sh
+++ scripts/fix-eth0-on-clone.sh
@@ -5,16 +5,19 @@
 
 # NOTE: Only works if only interface is present
 
-# 1 - Fix the /etc/udev/rules.d/70-persistent-net.rules
-grep -v "#" /etc/udev/rules.d/70-persistent-net.rules | tail -1 > /tmp/rule$$ 
-sed 's/NAME=.*/NAME="eth0"/' /tmp/rule$$ > /tmp/rule$$.tmp
-mv /tmp/rule$$.tmp /tmp/rule$$
-mv /tmp/rule$$ /etc/udev/rules.d/70-persistent-net.rules
-
-# 2.1 - Get the new mac address
+# Get the new mac address
 mac=$(ip link show | tail -1 | awk '{print $2}')
 
-# 2.2 Replace the new mac in the ifcfg config file
-sed 's/HWADDR.*/HWADDR='"$mac"'/' /etc/sysconfig/network-scripts/ifcfg-eth0 > /tmp/ifcfg$$
+# Only do the switch if the new mac is not aleready in the ifcfg file
+if [ $(grep -c $mac /etc/sysconfig/network-scripts/ifcfg-eth0) -eq 0 ]; then 
+    # 1 - Fix the /etc/udev/rules.d/70-persistent-net.rules
+    grep -v "#" /etc/udev/rules.d/70-persistent-net.rules | tail -1 > /tmp/rule$$ 
+    sed 's/NAME=.*/NAME="eth0"/' /tmp/rule$$ > /tmp/rule$$.tmp
+    mv /tmp/rule$$.tmp /tmp/rule$$
+    mv /tmp/rule$$ /etc/udev/rules.d/70-persistent-net.rules
 
-mv /tmp/ifcfg$$  /etc/sysconfig/network-scripts/ifcfg-eth0 
+    # 2 Replace the new mac in the ifcfg config file
+    sed 's/HWADDR.*/HWADDR='"$mac"'/' /etc/sysconfig/network-scripts/ifcfg-eth0 > /tmp/ifcfg$$
+
+    mv /tmp/ifcfg$$  /etc/sysconfig/network-scripts/ifcfg-eth0 
+fi
